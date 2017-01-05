ab2efabc38a4cef2339efcf8030df099901d8519
fswoaitian <337159934@qq.com>
2017-01-05 18:07:22 +0800

    BlacklistEditActivity

---
+++ app/src/main/java/com/itheima/mobilesafe/activity/BlacklistEditActivity.java
@@ -0,0 +1,15 @@
+package com.itheima.mobilesafe.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.itheima.mobilesafe.R;
+
+public class BlacklistEditActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_blacklist_edit);
+    }
+}
